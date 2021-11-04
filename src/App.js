daab710454161959baff92d1fca6b7d056f663af
Aal Emraan <developer.aalemraan@gmail.com>
2021-11-04 19:39:18 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes,  Route} from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home';
+import NavBar from './Pages/Shared/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <NavBar></NavBar>
+        <Home></Home>
+      </Router>
     </div>
   );
 }
