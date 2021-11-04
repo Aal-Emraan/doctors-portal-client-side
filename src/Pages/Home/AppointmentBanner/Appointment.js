dc5a6b654fb6fec12925dbc4e970a0690007fe92
Aal Emraan <developer.aalemraan@gmail.com>
2021-11-04 20:53:07 +0600

    updated

---
+++ src/Pages/Home/AppointmentBanner/Appointment.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import doctor from '../../../images/doctor.png';
+import bg from '../../../images/appointment-bg.png';
+import { Button, Typography } from '@mui/material';
+
+const Appointment = () => {
+    const appointmentBg = {
+        background: `url(${bg})`,
+        backgroundColor: 'rgba(45,58,74)',
+        marginTop: 100,
+        backgroundBlendMode: 'overlay'
+    }
+    return (
+        <Box style={appointmentBg} sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={6} md={5}>
+                    <img src={doctor} alt="" style={{width: '100%',marginTop: '-130px'}}/>
+                </Grid>
+                <Grid item xs={6} md={7} style={{color: 'white', marginTop: 100, textAlign: 'start'}}>
+                    <Typography variant="h6" color="info.main">
+                        Appointment
+                    </Typography>
+                    <Typography variant="h3" style={{margin: '30px 0'}}>
+                        Make An Appointment Today
+                    </Typography>
+                    <Typography variant="h6" style={{margin:"20px 0"}}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque obcaecati ipsam cum, necessitatibus corporis repudiandae molestiae sunt iure facere ipsa.
+                    </Typography>
+                    <Button variant="contained">Learn More</Button>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+};
+
+export default Appointment;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Services from '../Services/Services';
+import Appointment from './AppointmentBanner/Appointment';
 
 const Home = () => {
     return (
         <div>
             <Services></Services>
+            <Appointment></Appointment>
         </div>
     );
 };
