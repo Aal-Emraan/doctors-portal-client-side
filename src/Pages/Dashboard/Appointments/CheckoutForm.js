fec74d9e1fbd50993ef4f7828b9ee20ad94702cc
Aal Emraan <developer.aalemraan@gmail.com>
2021-11-16 13:38:13 +0600

    payment udpated

--- src/Pages/Dashboard/Appointments/CheckoutForm.js
+++ src/Pages/Dashboard/Appointments/CheckoutForm.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import React, { useState } from "react";
 
-const CheckoutForm = () => {
-    return (
-        <div>
-            <h2>this is checkout form</h2>
-        </div>
-    );
+const CheckoutForm = ({ appointment }) => {
+	const stripe = useStripe();
+	const elements = useElements();
+	const [error, setError] = useState("");
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		if (!stripe || !elements) {
+			return;
+		}
+
+		const card = elements.getElement(CardElement);
+		if (card === null) {
+			return;
+		}
+
+		const { error, paymentMethod } = await stripe.createPaymentMethod({
+			type: "card",
+			card,
+		});
+
+		if (error) {
+			console.log(error);
+			setError(error.message);
+		} else {
+			setError("");
+			console.log(paymentMethod);
+		}
+	};
+	return (
+		<form onSubmit={handleSubmit}>
+			<CardElement
+				options={{
+					style: {
+						base: {
+							fontSize: "16px",
+							color: "#424770",
+							"::placeholder": {
+								color: "#aab7c4",
+							},
+						},
+						invalid: {
+							color: "#9e2146",
+						},
+					},
+				}}
+			/>
+			<button type="submit" disabled={!stripe}>
+				Pay ${appointment.price}
+			</button>
+			{error && <p style={{ color: "red" }}>{error}</p>}
+		</form>
+	);
 };
 
 export default CheckoutForm;
--- src/Pages/Dashboard/Appointments/Pay.js
+++ src/Pages/Dashboard/Appointments/Pay.js
@@ -1,21 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe(
+	"pk_test_51Jw3LOBm7cLPALou4GtCFish5Xfg66AuqKzBB9plkZWE4nhHuecMZeU2YjJ27ovd1TlI4LKTtUNAuLeZ5Cs11nHk00mBcz6f6m"
+);
 
 const Pay = () => {
-    const {id} = useParams();
-    const [appointment, setAppointment] = useState({});
+	const { id } = useParams();
+	const [appointment, setAppointment] = useState({});
 
-    useEffect(() => {
-        fetch(`http://localhost:5000/appointment/${id}`)
-        .then(res => res.json())
-        .then(data => setAppointment(data))
-    },[id])
-    console.log(appointment);
-    return (
-        <div>
-            <h2>Pay your bill for {appointment?.price}</h2>
-        </div>
-    );
+	useEffect(() => {
+		fetch(`http://localhost:5000/appointment/${id}`)
+			.then((res) => res.json())
+			.then((data) => setAppointment(data));
+	}, [id]);
+	console.log(appointment);
+	return (
+		<div>
+			<h2>Pay your bill for {appointment?.price}</h2>
+			<Elements stripe={stripePromise}>
+				<CheckoutForm appointment={appointment} />
+			</Elements>
+		</div>
+	);
 };
 
 export default Pay;
